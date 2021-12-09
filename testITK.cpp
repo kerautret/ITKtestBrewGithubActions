e43b0ee0867d25765960277b1b6a17cb3237990b
Kerautret <bertrand.kerautret@univ-lyon2.fr>
2021-12-09 14:25:07 +0100

    init

---
+++ testITK.cpp
@@ -0,0 +1,13 @@
+  #include "itkImage.h"
+  #include <iostream>
+  
+  int main()
+  {
+    using ImageType = itk::Image< unsigned short, 3 >;
+   
+    ImageType::Pointer image = ImageType::New();
+  
+    std::cout << "ITK Hello World !" << std::endl;
+  
+    return EXIT_SUCCESS;
+  }
