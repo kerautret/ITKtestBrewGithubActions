84c804964db763effebb6185152b6d69a857609c
Kerautret <bertrand.kerautret@univ-lyon2.fr>
2021-12-17 15:04:45 +0100

    no ITK

--- testITK.cpp
+++ testITK.cpp
@@ -1,12 +1,11 @@
-  #include "itkImage.h"
+  #include <cmath>
   #include <iostream>
-  
+  using ::signbit;  
   int main()
   {
-    using ImageType = itk::Image< unsigned short, 3 >;
-   
-    ImageType::Pointer image = ImageType::New();
-  
+    //using ImageType = itk::Image< unsigned short, 3 >;
+    //ImageType::Pointer image = ImageType::New();
+
     std::cout << "ITK Hello World !" << std::endl;
   
     return EXIT_SUCCESS;
